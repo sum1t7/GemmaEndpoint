bb748e857e14b252e22404b2d10d88bba7dd4a60
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-06-06 17:00:18 +0530

    Mymoviedomain_update

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://take-movie-website.vercel.app/", 
+
+}));
 app.use(express.json({ limit: "10mb" }));
 
 const limiter = rateLimit({
