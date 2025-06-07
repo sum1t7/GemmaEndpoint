d9a606953391d2da7d77dbcc9745dd1e7170135b
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-06-07 08:40:55 +0530

    LimitJSON

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
- 
+app.use(cors({
+  origin: "https://take-movie-website.vercel.app", 
+
+  
+
+}));
+app.use(express.json({ limit: "10mb" }));
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 15,
